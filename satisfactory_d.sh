ea136a0b3b8a33303e7fd9eaaad682fec2912f1c
yingyi <jinzhongjia@qq.com>
2022-01-09 19:58:43 +0800

    测试

--- satisfactory_d.sh
+++ satisfactory_d.sh
@@ -9,8 +9,7 @@ root_need() {
         exit 1
     fi
 }
-
-#swap分区创建指令，一次性创建4G大小的swap分区用于扩展内存
+#交换分区函数
 swap_make() {
     #使用dd命令创建名为swapfile 的swap交换文件
     dd  if=/dev/zero  of=/var/swapfile  bs=1024  count=4194304 
@@ -23,16 +22,15 @@ swap_make() {
     #修改 fstab 配置，设置开机自动挂载该分区
     echo  "/var/swapfile   swap  swap  defaults  0  0" >>  /etc/fstab
 }
-
 #安装所需环境
 environment_get(){
-    apt update -y
-    apt install software-properties-common -y
     add-apt-repository multiverse
     dpkg --add-architecture i386
+    apt update -y
     apt install lib32gcc1 libcurl4-gnutls-dev:i386 lib32stdc++6 lib32z1 -y
+    apt install libsdl2-2.0-0:i386 -y
 }
-
+#判断steam用户函数
 steam_add(){
     egrep "^steam" /etc/passwd >& /dev/null
     if [ $? -ne 0 ]
@@ -52,10 +50,8 @@ root_need
 ########
 #配置检测
 ########
-
-
-#打印信息输出文件
-opath=/home/satisfactory_output.txt
+#设置log输出地址
+opath=/home/satisfactory_output.log
 # 获取物理内存总量
 pyhMem=`free | grep Mem | awk '{print $2}'`
 #获取虚拟内存总量
@@ -66,8 +62,8 @@ mem=`expr $virMem + $pyhMem`
 cpuNum=`grep -c "model name" /proc/cpuinfo`
 #获取本机ip地址
 ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
-#对文件进行覆写
-echo > $opath 2>&1
+#对文件进行覆写，置空
+:> $opath
 # 对内存大小进行判断
 if [ $mem -gt 6291456 ] 
 then
@@ -98,56 +94,59 @@ then
     read -r tmpNum
     if [ $tmpNum == 1 ]
     then
-        echo "接下来将会进行大量文件读写来创建swap分区，服务器响应可能会有卡顿！"
-        swap_make >> $opath 2>&1
+        echo "开始创建swap分区"
+        #这里会有输出信息
+        echo "swap分区开启输出信息：" >> $opath 2>&1
+        swap_make  >> $opath 2>&1
         echo "虚拟内存配置完成！"
     fi
 fi
 
-echo "接下来将安装steamCMD以及游戏所需环境"
-sleep 3s
+echo "开始安装steamCMD以及游戏所需环境"
 
 #安装steamCMD以及游戏所需环境
-echo "安装steamCMD以及游戏所需环境" >> $opath 2>&1
+
+#这里会有输出信息
+echo "steamcmd 及游戏环境安装输出：" >> $opath 2>&1
 environment_get >> $opath 2>&1
 
-
 echo "steamCMD以及游戏所需环境安装完成！"
-echo "接下来将安装steamCMD"
-sleep 3s
+echo "开始安装steamCMD"
 
-#此处为添加steam用户
 steam_add
 
 #进行steamCMD的安装
 fileDir="/home/steam"
 #新建游戏存放目录
-su - steam -c "mkdir ~/steamcmd" 
+if [ ! -d /home/steam/steamcmd  ];then
+    su - steam -c "mkdir ~/steamcmd"
+fi
 #下载文件
-su - steam -c "wget -P ~/steamcmd https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" 
+#这里有打印信息
+su - steam -c "wget -P ~/steamcmd https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" >> $opath 2>&1
 #解压文件
-su - steam -c "tar -zxvf ~/steamcmd/steamcmd_linux.tar.gz -C ~/steamcmd" 
+su - steam -c "tar -zxvf ~/steamcmd/steamcmd_linux.tar.gz -C ~/steamcmd" >> $opath 2>&1
 #删除下载的压缩包
-su - steam -c "rm -f ~/steamcmd/steamcmd_linux.tar.gz" 
-#运行steamCMD安装，并将信息输出到
-echo "运行steamCMD安装，并将信息输出到" >> $opath 2>&1
+su - steam -c "rm -f ~/steamcmd/steamcmd_linux.tar.gz"
+#运行steamCMD安装
+#这里有打印信息
 su - steam -c "~/steamcmd/steamcmd.sh +quit" >> $opath 2>&1
 
-mkdir -p /home/steam/.steam/sdk64/ 
+if [ ! -d /home/steam/.steam/sdk64/  ];then
+    mkdir -p /home/steam/.steam/sdk64/
+fi
 
-ln -s /home/steam/steamcmd/linux64/steamclient.so /home/steam/.steam/sdk64/ 
+ln -s /home/steam/steamcmd/linux64/steamclient.so /home/steam/.steam/sdk64/ >> $opath 2>&1
 
 echo "steamCMD安装完成！"
-echo "接下来将安装幸福工厂，3秒后开始安装"
-sleep 3s
+echo "开始安装幸福工厂"
 
-echo "安装两个环境" >> $opath 2>&1
-apt install libsdl2-2.0-0:i386 -y >> $opath 2>&1
-
-echo "幸福工厂安装" >> $opath 2>&1
+#这里有输出信息
 su - steam -c "~/steamcmd/steamcmd.sh +force_install_dir ~/SatisfactoryDedicatedServer +login anonymous +app_update 1690800 validate +quit" >> $opath 2>&1
 
-su - steam -c "mkdir -p ~/.config/Epic/FactoryGame/Saved/SaveGames/server" 
+if [ ! -d /home/steam/.config/Epic/FactoryGame/Saved/SaveGames/server  ];then
+    su - steam -c "mkdir -p ~/.config/Epic/FactoryGame/Saved/SaveGames/server"
+fi
 
 su - steam -c "echo "存档位置为："`pwd`"
 echo "创建启动脚本"
@@ -186,14 +185,10 @@ ExecStart=/home/steam/SatisfactoryDedicatedServer/start_server.sh
 
 [Install]
 WantedBy=multi-user.target
-EOF 
-
-echo "开机启动服务" >> $opath 2>&1
+EOF
 
 systemctl enable satisfactory.service >> $opath 2>&1
-
-systemctl start satisfactory.service 
-
+systemctl start satisfactory.service
 echo "安装完成！游戏服务已经启动！"
 echo "安装目录为："$fileDir
 echo "可以使用命令进行操作："
@@ -201,4 +196,4 @@ echo -e "本机ip为：""\033[;32m"$ip"\033[0m"
 echo -e "\033[;32m启动游戏：systemctl start satisfactory.service\033[0m"
 echo -e "\033[;32m重启游戏：systemctl restart satisfactory.service\033[0m"
 echo -e "\033[;32m关闭游戏：systemctl stop satisfactory.service\033[0m"
-echo "0 4 * * * systemctl restart satisfactory.service"  >>  /etc/crontab 
+echo "0 4 * * * systemctl restart satisfactory.service"  >>  /etc/crontab
